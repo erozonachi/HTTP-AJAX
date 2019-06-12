57bf43e78678ecd6cef49a34a938ad107b979962
erozonachi <erozonachi.james@gmail.com>
2019-06-12 13:44:08 +0100

    Create a get friends function
    Create a getFriends() function that uses axios to fetch list of friends
    from the API endpoint and updates the state with the fetched friends
    list.

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,7 +1,26 @@
 import React, { useState, useEffect, } from 'react';
+import Axios from 'axios';
 
 function App() {
   const [friends, setFriends] = useState([]);
+  const [errorMsg, setErrorMsg] = useState(``);
+  const [spinner, setSpinner] = useState(false);
+
+  const getFriends = () => {
+    setSpinner(true);
+
+    Axios(`http://localhost:5000/friends`)
+    .then(response => {
+      setFriends(response.data);
+    })
+    .catch(err => {
+      setErrorMsg(err.statusText);
+    })
+    .finally(() => {
+      setSpinner(false);
+    })
+  };
+
   return;
 }
 
