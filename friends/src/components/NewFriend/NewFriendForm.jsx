283eca71e223c15471a134a80a57f9f2561af9ac
erozonachi <erozonachi.james@gmail.com>
2019-06-12 14:45:06 +0100

    Crearte and use on change handler function

--- friends/src/components/NewFriend/NewFriendForm.jsx
+++ friends/src/components/NewFriend/NewFriendForm.jsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useState, } from 'react';
 
 export default function NewFriendForm(props) {
+  const [friend, setFriend] = useState({name: '', age: '', email: '',});
+
+  const handleOnChange = (event) => {
+    const friendData = friend;
+
+    if(event.target.placeholder.includes(`Name`)) {
+      friendData.name = event.target.value;
+      setFriend(friendData);
+    }
+    if(event.target.placeholder.includes(`Age`)) {
+      friendData.age = event.target.value;
+      setFriend(friendData);
+    }
+    if(event.target.placeholder.includes(`Email`)) {
+      friendData.email = event.target.value;
+      setFriend(friendData);
+    }
+  }
+
   return(
     <div>
       <h2>Add New Friend</h2>
       <form>
-        <input placeholder='Enter Name' />
-        <input placeholder='Enter Age' />
-        <input placeholder='Enter Email' />
+        <input onChange={handleOnChange} value={friend.name} placeholder='Enter Name' />
+        <input onChange={handleOnChange} value={friend.age} placeholder='Enter Age' />
+        <input onChange={handleOnChange} value={friend.email} placeholder='Enter Email' />
       </form>
     </div>
   );
