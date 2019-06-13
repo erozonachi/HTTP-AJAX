e5188323e44f0dcbe1c011940f3fca3368f4f9ab
erozonachi <erozonachi.james@gmail.com>
2019-06-13 19:52:17 +0100

    Refactor new-friend-form to use refs
    Refactor new-friend-form to use refs, and remove unused lines of code

--- friends/src/components/NewFriend/NewFriendForm.jsx
+++ friends/src/components/NewFriend/NewFriendForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, } from 'react';
+import React, { useEffect, } from 'react';
 import FormContainer from './StyledComponents/FormContainer';
 
 export default function NewFriendForm(props) {
@@ -9,38 +9,31 @@ export default function NewFriendForm(props) {
     email: '',
     sex: '',
   };
-  const initialFormState = (id? props.getFriend(id) : initialFriendState);
+  const initialVals = (id? props.getFriend(id) : initialFriendState);
 
-  const [friend, setFriend] = useState(initialFormState);
+  const nameInput = React.createRef();
+  const ageInput = React.createRef();
+  const sexInput = React.createRef();
+  const emailInput = React.createRef();
 
   useEffect(() => {
-    return () => {
-      if (props.match.params.id.trim()) {
-        setFriend({...initialFriendState});
-      }
-    }
-  }, [props.match.params.id, initialFriendState]);
-
-  const handleOnChange = (event) => {
-
-    if(event.target.placeholder.includes(`Name`)) {
-      setFriend({...friend, name: event.target.value,});
-    }
-    if(event.target.placeholder.includes(`Age`)) {
-      setFriend({...friend, age: event.target.value,});
-    }
-    if(event.target.placeholder.includes(`Email`)) {
-      setFriend({...friend, email: event.target.value,});
-    }
-    if(event.target.placeholder.includes(`Gender`)) {
-      setFriend({...friend, sex: event.target.value,});
-    }
-  }
+    nameInput.current.value = initialVals.name;
+    ageInput.current.value = initialVals.age;
+    sexInput.current.value = initialVals.sex;
+    emailInput.current.value = initialVals.email;
+  });
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    if (friend.name && friend.age && friend.email && friend.sex) {
+    if (nameInput.current.value && ageInput.current.value && 
+      emailInput.current.value && sexInput.current.value) {
+      const friend = {
+        name: nameInput.current.value,
+        age: ageInput.current.value,
+        sex: sexInput.current.value,
+        email: emailInput.current.value,
+      }
       id? props.editSubmitHandler(friend, id) : props.addSubmitHandler(friend);
       id = null;
       props.history.push('/');
@@ -51,10 +44,18 @@ export default function NewFriendForm(props) {
     <FormContainer>
       <h2>{props.match.params.id.trim()? 'Edit Existing Friend' : 'Add New Friend'}</h2>
       <form onSubmit={handleSubmit}>
-        <input onChange={handleOnChange} value={friend.name} placeholder='Enter Name' />
-        <input onChange={handleOnChange} value={friend.age} placeholder='Enter Age' />
-        <input onChange={handleOnChange} value={friend.sex} placeholder='Enter Gender' />
-        <input onChange={handleOnChange} value={friend.email} placeholder='Enter Email' />
+        <input  
+          ref={nameInput} 
+          placeholder='Enter Name' />
+        <input 
+          ref={ageInput} 
+          placeholder='Enter Age' />
+        <input 
+          ref={sexInput} 
+          placeholder='Enter Gender' />
+        <input 
+          ref={emailInput} 
+          placeholder='Enter Email' />
         <button type='submit'>{props.match.params.id.trim()? 'Edit Friend' : 'Add Friend'}</button>
       </form>
     </FormContainer>
