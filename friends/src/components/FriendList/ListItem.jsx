c3ac370a1b406a13ef90ba5920276016127bd1cf
erozonachi <erozonachi.james@gmail.com>
2019-06-13 16:28:04 +0100

    Create and use handle delete function in list item

--- friends/src/components/FriendList/ListItem.jsx
+++ friends/src/components/FriendList/ListItem.jsx
@@ -3,12 +3,19 @@ import FriendItem from './StyledComponents/FriendItem';
 import { Link, } from 'react-router-dom';
 
 export default function ListItem(props) {
+  const handleDelete = () => {
+    props.delHandler(props.friend.id);
+  }
+
   return(
     <FriendItem>
       <h3>{props.friend.name}</h3>
       <span><em>Age:&nbsp;</em>{props.friend.age}</span>
       <span><em>Email:&nbsp;</em>{props.friend.email}</span>
-      <div><Link to={`/new_friend/${props.friend.id}`}><button>Edit</button></Link></div>
+      <div>
+        <Link to={`/new_friend/${props.friend.id}`}><button>Edit</button></Link>
+        <button onClick={handleDelete}>Delete</button>
+      </div>
     </FriendItem>
   );
 }
