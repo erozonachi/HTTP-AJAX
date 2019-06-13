00b749d636840d691e33e42e14f36f36bd98336e
erozonachi <erozonachi.james@gmail.com>
2019-06-13 13:30:11 +0100

    Tweak friend item and list components styles

--- friends/src/components/FriendList/StyledComponents/FriendItem.js
+++ friends/src/components/FriendList/StyledComponents/FriendItem.js
@@ -5,20 +5,21 @@ const FriendItem = styled.li`
   flex-direction: column;
   justify-content: center;
   align-item: baseline;
-  width: 50%;
+  width: 45%;
   background-color: white;
   color: #555;
+  margin: .5em 0;
   box-shadow: 4px 4px 4px 4px #ccc;
   > h3 {
     font-size: 1.8rem;
     font-weight: bold;
-    padding: 1em 0;
+    padding: .5em 0;
     width: 100%;
     text-align: center;
   }
   > span {
     display: flex;
-    padding: .5em 0;
+    padding: .5em;
     > em {
       font-weight: bold;
     }
