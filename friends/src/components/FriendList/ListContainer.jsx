5892f0e9bac141d8a27f2b9178e0e2744593c08a
erozonachi <erozonachi.james@gmail.com>
2019-06-13 13:32:12 +0100

    Remove unused lines of code from list-container

--- friends/src/components/FriendList/ListContainer.jsx
+++ friends/src/components/FriendList/ListContainer.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ListItem from './ListItem';
-import FriendItem from './StyledComponents/FriendItem';
 import FriendsList from './StyledComponents/FriendsList';
 
 export default function ListContainer(props) {
@@ -8,11 +7,6 @@ export default function ListContainer(props) {
     <FriendsList>
       <h2>Friends List</h2>
       <ul>
-        <FriendItem head={true}>
-          <span>Name</span>
-          <span>Age</span>
-          <span>Email</span>
-        </FriendItem>
         {props.friends.map(friend => <ListItem key={`${friend.id}`} friend={friend} />)}
       </ul>
     </FriendsList>
