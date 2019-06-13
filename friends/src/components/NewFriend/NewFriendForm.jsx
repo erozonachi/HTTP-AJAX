de36e7aa82dc42850887e5dbffbe036a24d4af78
erozonachi <erozonachi.james@gmail.com>
2019-06-13 15:23:29 +0100

    Use object to hold form state

--- friends/src/components/NewFriend/NewFriendForm.jsx
+++ friends/src/components/NewFriend/NewFriendForm.jsx
@@ -2,31 +2,33 @@ import React, { useState, } from 'react';
 import FormContainer from './StyledComponents/FormContainer';
 
 export default function NewFriendForm(props) {
-  const [name, setName] = useState('');
-  const [age, setAge] = useState('');
-  const [email, setEmail] = useState('');
+  const initialFormState = {
+    name: '',
+    age: '',
+    email: '',
+  };
+
+  const [friend, setFriend] = useState(initialFormState);
 
   const handleOnChange = (event) => {
 
     if(event.target.placeholder.includes(`Name`)) {
-      setName(event.target.value);
+      setFriend({...friend, name: event.target.value,});
     }
     if(event.target.placeholder.includes(`Age`)) {
-      setAge(event.target.value);
+      setFriend({...friend, age: event.target.value,});
     }
     if(event.target.placeholder.includes(`Email`)) {
-      setEmail(event.target.value);
+      setFriend({...friend, email: event.target.value,});
     }
   }
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    if (name && age && email) {
-      props.addSubmitHandler({name: name, age: age, email: email});
-      setName('');
-      setAge('');
-      setEmail('');
+    if (friend.name && friend.age && friend.email) {
+      props.addSubmitHandler(friend);
+      setFriend(initialFormState);
     }
   }
 
@@ -34,9 +36,9 @@ export default function NewFriendForm(props) {
     <FormContainer>
       <h2>{props.match.params.id.trim()? 'Edit Existing Friend' : 'Add New Friend'}</h2>
       <form onSubmit={handleSubmit}>
-        <input onChange={handleOnChange} value={name} placeholder='Enter Name' />
-        <input onChange={handleOnChange} value={age} placeholder='Enter Age' />
-        <input onChange={handleOnChange} value={email} placeholder='Enter Email' />
+        <input onChange={handleOnChange} value={friend.name} placeholder='Enter Name' />
+        <input onChange={handleOnChange} value={friend.age} placeholder='Enter Age' />
+        <input onChange={handleOnChange} value={friend.email} placeholder='Enter Email' />
         <button type='submit'>{props.match.params.id.trim()? 'Edit Friend' : 'Add Friend'}</button>
       </form>
     </FormContainer>
